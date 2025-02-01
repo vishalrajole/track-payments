4cc2acde76ce16dcdc28c0cefea736e9910617d8
vishalrajole <vishalrajole1991@gmail.com>
2025-02-01 22:10:25 +0100

    feat: deployed nest js on vercel to test

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3001/";
+// export const BASE_URL = "http://localhost:3001/";
+export const BASE_URL =
+  "https://backend-5tdwvfsie-vishalrajoles-projects.vercel.app/";
 export const STALE_TIME = 1000 * 60 * 1;
